512a7c3bce498c9b316cda7f00b26f85b5a734c2
xiong6456 <hevinxiong@163.com>
2018-04-28 12:01:28 +0800

    这是我提交的第一个版本

---
+++ src/main/java/com/zero/springbootframe/SpringbootframeApplication.java
@@ -0,0 +1,12 @@
+package com.zero.springbootframe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootframeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootframeApplication.class, args);
+    }
+}
